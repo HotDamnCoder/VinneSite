335b7647a92ddeb277ce224de4014a795d6c12cc
HotDamnCoder <marcusb2kl@gmail.com>
2019-12-15 00:07:51 +0200

    Update ALLOWED_HOSTS with site and development server URL

--- VinneSite/settings.py
+++ VinneSite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', ***REMOVED***)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = [['vinne.herokuapp.com','127.0.0.1']]
+ALLOWED_HOSTS = ['vinne.herokuapp.com','127.0.0.1']
 
 
 # Application definition
