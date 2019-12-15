e5809f7db8092738dc33fcad6b07b47b80a07b47
HotDamnCoder <marcusb2kl@gmail.com>
2019-12-15 13:49:40 +0200

    Added more in-depth language control

--- MainVinne/EST_urls.py
+++ MainVinne/EST_urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import EST_views
 app_name = "MainVinne"
 urlpatterns = [
-    path('', EST_views.Home.as_view(), name="main"),
-    path('elektronSkeemer/', EST_views.Skeemer.as_view(), name="results"),
-    path('otsing/', EST_views.search.as_view(), name="search"),
-    path('harjutamine/', EST_views.harjutama.as_view(), name="harjutama"),
-    path('<str:element>/', EST_views.Element.as_view(), name="element")
+    path('', EST_views.Home.as_view(), name="est_main"),
+    path('elektronSkeemer/', EST_views.Skeemer.as_view(), name="est_skeemer"),
+    path('otsing/', EST_views.search.as_view(), name="est_search"),
+    path('harjutamine/', EST_views.harjutama.as_view(), name="est_harjutama"),
+    path('<str:element>/', EST_views.Element.as_view(), name="est_element")
 ]
