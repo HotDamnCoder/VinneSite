24365caf5219fe6b2285cdfa22b2fdf08eebaf4e
HotDamnCoder <marcusb2kl@gmail.com>
2019-12-15 13:56:18 +0200

    Added more in-depth language control

--- MainVinne/ENG_urls.py
+++ MainVinne/ENG_urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
+"""from django.urls import path
 
 from . import ENG_views
 app_name = "MainVinne"
 urlpatterns = [
-    """
+
     path('', ENG_views.Home.as_view(), name="main"),
-    path('result/', ENG_views.Results.as_view(), name="results"),
-    path('search/', ENG_views.search.as_view(), name="search"),
-    path('harjutamine/', ENG_views.harjutama.as_view(), name="harjutama"),
-    path('<str:element>/', ENG_views.Element.as_view(), name="element")"""
-]
+    path('electronConfigurator/', ENG_views.Results.as_view(), name="eng_skeemer"),
+    path('search/', ENG_views.search.as_view(), name="eng_search"),
+    path('practise/', ENG_views.harjutama.as_view(), name="eng_harjutama"),
+    path('<str:element>/', ENG_views.Element.as_view(), name="eng_element")
+]"""
