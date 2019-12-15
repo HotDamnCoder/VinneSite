b17421c034fd2f6eb25110141bb174e67da452f7
HotDamnCoder <marcusb2kl@gmail.com>
2019-12-15 14:24:28 +0200

    Added more in-depth language control

--- MainVinne/views.py
+++ MainVinne/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import View
 from django.shortcuts import redirect
 from django.urls import reverse
+from .EST_views import Home
 class redirect(View):
     def get(self, request):
-        return redirect(reverse("MainVinne:eng_main"))
+        return redirect(reverse(Home))
