d532223aaf8ecf10e0f30437f2778da4b301ca57
HotDamnCoder <marcusb2kl@gmail.com>
2019-12-15 15:00:35 +0200

    Added more in-depth language control

--- MainVinne/urls.py
+++ MainVinne/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from . import views
-app_name = "MainVinne"
+app_name = "Global_MainVinne"
 urlpatterns = [
-    path('', views.redirect.as_view(), "redirect")
+    path('', views.redirect.as_view(), name="redirect")
 ]
