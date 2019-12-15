5713bff25f2e6a9a5e9372ae32a75164d0b9de2a
HotDamnCoder <marcusb2kl@gmail.com>
2019-12-15 13:31:08 +0200

    Added english support

---
+++ MainVinne/est_urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+app_name = "MainVinne"
+urlpatterns = [
+    path('', views.MainSite.as_view(), name="main"),
+    path('result/', views.Results.as_view(), name="results"),
+    path('search/', views.search.as_view(), name="search"),
+    path('harjutamine/', views.harjutama.as_view(), name="harjutama"),
+    path('<str:element>/', views.Element.as_view(), name="element")
+]
--- VinneSite/urls.py
+++ VinneSite/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('MainVinne.urls')),
+    path('ee/', include('MainVinne.est_urls')),
 
 ]
