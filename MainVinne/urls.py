8dc7cd1817ff401d899f5dd1902539f5e240e0da
HotDamnCoder <marcusb2kl@gmail.com>
2019-12-15 13:28:06 +0200

    Added english support

--- MainVinne/urls.py
+++ MainVinne/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from . import views
 app_name = "MainVinne"
 urlpatterns = [
-    path('', views.MainSite.as_view(), name="main"),
-    path('result/', views.Results.as_view(), name="results"),
-    path('search/', views.search.as_view(), name="search"),
-    path('harjutamine/', views.harjutama.as_view(), name="harjutama"),
-    path('<str:element>/', views.Element.as_view(), name="element")
+
+    path('ee/', views.MainSite.as_view(), name="main"),
+    path('ee/result/', views.Results.as_view(), name="results"),
+    path('ee/search/', views.search.as_view(), name="search"),
+    path('ee/harjutamine/', views.harjutama.as_view(), name="harjutama"),
+    path('ee/<str:element>/', views.Element.as_view(), name="element")
 
 
 ]
